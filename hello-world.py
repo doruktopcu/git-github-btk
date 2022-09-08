7aa58933666387acc0f6d23274e20884fd9a9867
Doruk Topcu <doruktopcu5@gmail.com>
2022-09-08 19:17:59 +0300

    New function added, for elementary number theory.

--- hello-world.py
+++ hello-world.py
@@ -9,3 +9,20 @@ for i in range(8):
             a_new_array[i,j] = k
 
 print(a_new_array)
+
+#We created a random matrix, good job!
+
+def a_new_function(takes_integer, modulo_integer):
+    finds_the_remainder = takes_integer % modulo_integer
+    possible_numbers_list = list()
+
+    for k in range(73):
+        finds_the_possible_number = finds_the_remainder + k * modulo_integer
+        possible_numbers_list.append(finds_the_possible_number)
+
+    return possible_numbers_list
+
+we_asked_for_it = a_new_function(100,3)
+print(we_asked_for_it)
+
+#Congratz, now you have a list of numbers which has the same remainder, according to the number and the modulu you have entered.
